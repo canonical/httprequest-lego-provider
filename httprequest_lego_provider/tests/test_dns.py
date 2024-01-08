a09c502a6ff07551ee9701c6d9e79c3909b2a8d3
arturo-seijas <arturo.seijas@canonical.com>
2024-01-08 10:19:08 +0100

    minor refactor

--- httprequest_lego_provider/tests/test_dns.py
+++ httprequest_lego_provider/tests/test_dns.py
@@ -11,25 +11,26 @@ from git import GitCommandError, Repo
 from httprequest_lego_provider.dns import DnsSourceUpdateError, remove_dns_record, write_dns_record
 
 
-@patch.object(Path, "exists", False)
+@patch.object(Path, "exists")
 @patch.object(Repo, "clone_from")
-def test_write_dns_record_raises_exception(repo_patch):
+def test_write_dns_record_raises_exception(repo_patch, patch_path):
     """
     arrange: mock the repo so that it raises a GitCommandError.
     act: attempt to write a new DNS record.
     assert: a DnsSourceUpdateError exception is raised.
     """
     repo_patch.side_effect = GitCommandError("Error executing command")
+    patch_path.return_value = True
 
     dns_record = "site.example.com"
     with pytest.raises(DnsSourceUpdateError):
         write_dns_record(dns_record)
 
 
-@patch.object(Path, "exists", False)
+@patch.object(Path, "exists")
 @patch.object(Path, "write_text")
 @patch.object(Repo, "clone_from")
-def test_write_dns_record_if_not_exists(repo_patch, _):
+def test_write_dns_record_if_not_exists(repo_patch, _, patch_path):
     """
     arrange: mock the repo and filesystem so that the file matching a DNS record doesn't exist.
     act: attempt to write a new DNS record.
@@ -37,6 +38,7 @@ def test_write_dns_record_if_not_exists(repo_patch, _):
     """
     repo_mock = MagicMock(spec=Repo)
     repo_patch.return_value = repo_mock
+    patch_path.return_value = False
 
     dns_record = "site.example.com"
     write_dns_record(dns_record)
@@ -46,9 +48,9 @@ def test_write_dns_record_if_not_exists(repo_patch, _):
     repo_mock.remote(name="origin").push.assert_called_once()
 
 
-@patch.object(Path, "exists", True)
+@patch.object(Path, "exists")
 @patch.object(Repo, "clone_from")
-def test_write_dns_record_if_exists(repo_patch):
+def test_write_dns_record_if_exists(repo_patch, patch_path):
     """
     arrange: mock the repo and filesystem so that the file matching a DNS record exists.
     act: attempt to write a new DNS record.
@@ -56,6 +58,7 @@ def test_write_dns_record_if_exists(repo_patch):
     """
     repo_mock = MagicMock(spec=Repo)
     repo_patch.return_value = repo_mock
+    patch_path.return_value = True
 
     dns_record = "site.example.com"
     write_dns_record(dns_record)
@@ -63,24 +66,25 @@ def test_write_dns_record_if_exists(repo_patch):
     repo_mock.remote(name="origin").push.assert_not_called()
 
 
-@patch.object(Path, "exists", False)
+@patch.object(Path, "exists")
 @patch.object(Repo, "clone_from")
-def test_remove_dns_record_raises_exception(repo_patch):
+def test_remove_dns_record_raises_exception(repo_patch, patch_path):
     """
     arrange: mock the repo so that it raises a GitCommandError.
     act: attempt to remove a DNS record.
     assert: a DnsSourceUpdateError exception is raised.
     """
     repo_patch.side_effect = GitCommandError("Error executing command")
+    patch_path.return_value = False
 
     dns_record = "site.example.com"
     with pytest.raises(DnsSourceUpdateError):
         remove_dns_record(dns_record)
 
 
-@patch.object(Path, "exists", False)
+@patch.object(Path, "exists")
 @patch.object(Repo, "clone_from")
-def test_remove_dns_record_if_not_exists(repo_patch):
+def test_remove_dns_record_if_not_exists(repo_patch, patch_path):
     """
     arrange: mock the repo and filesystem so that the file matching a DNS doesn't exist.
     act: attempt to delete a new DNS record.
@@ -88,6 +92,7 @@ def test_remove_dns_record_if_not_exists(repo_patch):
     """
     repo_mock = MagicMock(spec=Repo)
     repo_patch.return_value = repo_mock
+    patch_path.return_value = False
 
     dns_record = "site.example.com"
     remove_dns_record(dns_record)
@@ -95,10 +100,10 @@ def test_remove_dns_record_if_not_exists(repo_patch):
     repo_mock.remote(name="origin").push.assert_not_called()
 
 
-@patch.object(Path, "exists", True)
+@patch.object(Path, "exists")
 @patch.object(Path, "unlink")
 @patch.object(Repo, "clone_from")
-def test_remove_dns_record_if_exists(repo_patch, _):
+def test_remove_dns_record_if_exists(repo_patch, _, patch_path):
     """
     arrange: mock the repo and filesystem so that the file matching a DNS exists.
     act: attempt to delete a new DNS record.
@@ -106,6 +111,7 @@ def test_remove_dns_record_if_exists(repo_patch, _):
     """
     repo_mock = MagicMock(spec=Repo)
     repo_patch.return_value = repo_mock
+    patch_path.return_value = True
 
     dns_record = "site.example.com"
     remove_dns_record(dns_record)
