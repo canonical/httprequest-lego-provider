35ab12f5bfb041e71651199ee7a30409eb137dfe
Arturo Seijas <arturo.seijas@canonical.com>
2024-01-24 10:38:39 +0100

    Add unit tests

--- app/urls.py
+++ app/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/v1/auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
--- httprequest_lego_provider/tests/test_views.py
+++ httprequest_lego_provider/tests/test_views.py
@@ -233,3 +233,27 @@ def test_get_cleanup_when_logged_in(client: Client, user_auth_token: str):
     response = client.get("/present/", headers={"AUTHORIZATION": f"Basic {user_auth_token}"})
 
     assert response.status_code == 405
+
+@pytest.mark.django_db
+def test_test_jwt_token_login(
+    client: Client, username: str, user_password: str, domain_user_permission: DomainUserPermission
+):
+    """
+    arrange: mock the write_dns_recod method, log in a user and give him permissions on a FQDN.
+    act: submit a POST request for the present URL containing the fqdn above.
+    assert: a 204 is returned.
+    """
+    response = client.post(
+        "/api/v1/auth/token/",
+        data={"username": username, "password": user_password},
+    )
+
+    with patch("httprequest_lego_provider.views.write_dns_record") as mocked_dns_write:
+        value = secrets.token_hex()
+        response = client.post(
+            "/present/",
+            data={"fqdn": domain_user_permission.domain.fqdn, "value": value},
+            headers={"AUTHORIZATION": f"JWT {response.token}"},
+        )
+
+        assert response.status_code == 204
