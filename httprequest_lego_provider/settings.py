e1645f7459b3319b3ed94efbdccf963598a20cac
Tom Haddon <tom.haddon@canonical.com>
2024-09-16 17:14:39 +0200

    dashes are converted to underscores for env variables

--- httprequest_lego_provider/settings.py
+++ httprequest_lego_provider/settings.py
@@ -4,6 +4,6 @@
 
 import os
 
-GIT_REPO_URL = os.getenv("DJANGO_GIT-REPO", default="")
-GIT_SSH_KEY = os.getenv("DJANGO_GIT-SSH-KEY", default="")
+GIT_REPO_URL = os.getenv("DJANGO_GIT_REPO", default="")
+GIT_SSH_KEY = os.getenv("DJANGO_GIT_SSH_KEY", default="")
 LOGIN_REDIRECT_URL = "/"
